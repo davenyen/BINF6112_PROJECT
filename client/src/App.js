6b204655583cc60bd8d77590065b2cfdaf46226d
davenyen <davidnguyen4898@gmail.com>
2020-10-14 14:43:38 +1100

    full-stack template

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav.js';
 
-export default function App() {
-  return (
-    <Router>
-      <Switch>
+export default class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = { apiResponse: "" };
+    } 
+
+  callAPI() {
+      fetch("http://localhost:9000/testAPI")
+          .then(res => res.text())
+          .then(res => this.setState({ apiResponse: res }))
+          .catch(err => err);
+  }
+
+  componentDidMount() {
+      this.callAPI();
+  }
+
+  render() {
+      return (
+          <div className="App">
+              <p className="App-intro">{this.state.apiResponse}</p>
+          </div>
+      );
+  }
+}
+
+/*
+<Switch>
         <Nav />
         <Route path='/home'/>
         <Route path='/results'/>
       </Switch>
-    </Router>
-  );
-}
+*/
