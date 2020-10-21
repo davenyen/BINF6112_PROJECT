95c303ba88c2169edd886be5495e7a27e42f19f0
davenyen <davidnguyen4898@gmail.com>
2020-10-22 03:26:08 +1100

    Refactored, removed upload button for submit, fixed empty submit bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,150 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
-import { OutTable, ExcelRenderer } from 'react-excel-renderer';
-import { Col, Input, InputGroup, InputGroupAddon, FormGroup, Label, Button, Fade, FormFeedback, Container, Card } from 'reactstrap';
-import axios from 'axios';
+import { Container, Card } from 'reactstrap';
 import Navbar from './components/Navbar';
-import Table from './components/Table';
 import UploadForm from './components/UploadForm';
 import Tabs from "./components/Tabs"; 
-// todo: 
-// - convert to hooks 
-// - split into different components
-
-const apiURL = "http://localhost:8000";
+import { OutTable } from 'react-excel-renderer';
+import Table from './components/Table';
 
 export default class App extends Component {
   constructor(){
     super();
     this.state={
-        apiResponse: "",
-        isOpen: false,
         dataLoaded: false,
-        isFormInvalid: false,
-        fileLimitExceeded: false,
-        fileObject: null,
         rows: null,
         cols: null,
         processedData: null
     }
-    this.fileHandler = this.fileHandler.bind(this);
-    this.toggle = this.toggle.bind(this);
-    this.openFileBrowser = this.openFileBrowser.bind(this);
-    this.renderFile = this.renderFile.bind(this);
-    this.fileInput = React.createRef();
   }
 
-    // callAPI() {
-    // fetch("http://localhost:9000/testAPI")
-    //     .then(res => res.text())
-    //     .then(res => this.setState({ apiResponse: res }))
-    //     .catch(err => err);
-    // }
-
-    // componentDidMount() {
-    //     this.callAPI();
-    // }
-
-    // Loads and renders file to client
-    renderFile = (fileObj) => {
-        //just pass the fileObj as parameter
-        ExcelRenderer(fileObj, (err, resp) => {
-        if(err){
-            console.log(err);            
-        }
-        else{
-            this.setState({
-            dataLoaded: true,
-            cols: resp.cols,
-            rows: resp.rows
-            });
-        }
-        }); 
-    }
-
-  // Checks if file is valid
-  fileHandler = (event) => {    
-    if(event.target.files.length){
-        this.setState({
-            fileObject: event.target.files
-        });
-        let fileObj = event.target.files;
-        let uploadedFileNames = [];
-        if(event.target.files.length < 3){
-          for (var i = 0; i < fileObj.length; i++) {
-              let fileName = fileObj[i].name;
-              if(fileName.slice(fileName.lastIndexOf('.')+1) === "xlsx" || fileName.slice(fileName.lastIndexOf('.')+1) === "gpr" ){
-                uploadedFileNames.push(fileName);
-                this.setState({
-                    // uploadedFileNames: uploadedFileNames,
-                    isFormInvalid: false
-                });
-                this.renderFile(fileObj[i])
-              } else{
-                this.setState({
-                    isFormInvalid: true,
-                    uploadedFileNames: []
-                })
-                break;
-              }   
-          }
-        }else{
-          this.setState({
-            fileLimitExceeded: true,
-            uploadedFileNames: []
-          })
-        }
-
-
-        //check for file extension and pass only if it is .xlsx/gpr and display error message otherwise
-        if (uploadedFileNames.length > 0) {
-          this.setState({
-            uploadedFileNames: uploadedFileNames.join(", ")
-          })
-        }               
-    }
-  }
-
-  // Backend incorporation
-  onClickHandler = () => {
-    const data = new FormData();
-    if (this.fileObject == null) return;
-    for(var x = 0; x<this.state.fileObject.length; x++) {
-      data.append('file', this.state.fileObject[x])
-    }
-    axios.post(apiURL + '/upload', data, {
-        
-    }).then(res => {
-        // console.log(res);
-        if (res.status === 200) {
-          axios.get(apiURL+"/process")
-              .then(rsp => rsp.data)
-              .then(json => {
-                this.setState({
-                  processedData: json,
-                  dataLoaded: false
-                });
-              });
-        }
-    }).catch(err => console.log(err))
-  }
-
-  toggle() {
+  // Passed as a method to child in order for child to update parent state
+  handleSubmit = (dataLoaded, rows, cols, processedData) => {
     this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-
-  openFileBrowser = () => {
-    this.fileInput.current.click();
-  }
+        dataLoaded: dataLoaded,
+        rows: rows,
+        cols: cols,
+        processedData: processedData
+    })
+  };
 
   render() {
     return (
       <div>
         <Navbar />
         <Container>
-        <div>
+        <div className="analysis-tabs">
         <h2>Plase choose your analysis modes</h2>
         <Tabs> 
           <div label="Single Sample"> 
@@ -158,54 +47,35 @@ export default class App extends Component {
           </div> 
         </Tabs> 
         </div>
-        <form className="xlsx/gpr-form">
-          <FormGroup row>
-            <Label for="exampleFile" xs={6} sm={4} lg={2} size="lg">Upload xlsx/gpr</Label>          
-            <Col xs={4} sm={8} lg={10}>                                                     
-              <InputGroup>
-                <InputGroupAddon addonType="prepend">
-                    <Button color="info" style={{color: "white", zIndex: 0}} onClick={this.openFileBrowser.bind(this)}>
-                        <i className="cui-file"></i>
-                        Browse&hellip;
-                    </Button>
-                  <input type="file" hidden onChange={this.fileHandler.bind(this)} ref={this.fileInput} multiple onClick={(event)=> { event.target.value = null }} style={{"padding":"10px"}} />      
-                  <button type="button" className="btn btn-success btn-block" onClick={this.onClickHandler}>
-                      Upload
-                  </button>
-                </InputGroupAddon>
-                <Input type="text" className="form-control" value={this.state.uploadedFileNames} readOnly invalid={this.state.isFormInvalid || this.state.fileLimitExceeded} />                                              
-                <FormFeedback>    
-                  <Fade in={this.state.isFormInvalid} tag="h6" style={{fontStyle: "italic"}}>
-                    Please select a .xlsx/.gpr file only !
-                  </Fade>
-                  <Fade in={this.state.fileLimitExceeded} tag="h6" style={{fontStyle: "italic"}}>
-                    Maximum of 2 data files allowed!
-                  </Fade>                                                                 
-                </FormFeedback>
-              </InputGroup>     
-            </Col>                                                   
-            </FormGroup>
-        </form>
-        <UploadForm/>
+        <UploadForm 
+          className="xlsx/gpr-form" 
+          fileTypeOne="xlsx" 
+          fileTypeTwo="gpr"
+          warningOne="Please select a .xlsx/.gpr file only!" 
+          warningTwo="Maximum of 2 data files allowed!"
+          handleSubmit={this.handleSubmit}
+        />
+        <UploadForm 
+          className="pdb-form" 
+          fileTypeOne="pdb" 
+          fileTypeTwo=""
+          warningOne="Please select a pdb file only!" 
+          warningTwo="Only 1 pdb file allowed!"
+        />
         {this.state.dataLoaded && 
         <div className="output-table">
           <Card body outline color="secondary" className="restrict-card">
-              <OutTable data={this.state.rows} columns={this.state.cols} tableClassName="ExcelTable2007" tableHeaderRowClass="heading" />
+              <OutTable 
+                data={this.state.rows} 
+                columns={this.state.cols} 
+                tableClassName="ExcelTable2007" 
+                tableHeaderRowClass="heading" 
+              />
           </Card>  
         </div>}
-        {this.state.processedData && 
-          <Table data={this.state.processedData} />
-        }
+        {this.state.processedData && <Table data={this.state.processedData} />}
         </Container>
       </div>
     );
   }
 }
-
-/*
-<Switch>
-        <Nav />
-        <Route path='/home'/>
-        <Route path='/results'/>
-      </Switch>
-*/
--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { ExcelRenderer } from 'react-excel-renderer';
 import axios from 'axios';
 import { Col, Input, InputGroup, InputGroupAddon, FormGroup, Label, Button, Fade, FormFeedback } from 'reactstrap';
+import { ExcelRenderer } from 'react-excel-renderer';
 
 const apiURL = "http://localhost:8000";
 
@@ -54,10 +54,10 @@ export default class UploadForm extends Component {
             if(fileObj.length === 1){
               for (var i = 0; i < fileObj.length; i++) {
                   let fileName = fileObj[i].name;
-                  if(fileName.slice(fileName.lastIndexOf('.')+1) === "pdb"){
+                  if (fileName.slice(fileName.lastIndexOf('.')+1) === this.props.fileTypeOne ||
+                    fileName.slice(fileName.lastIndexOf('.')+1) === this.props.fileTypeTwo ){
                     uploadedFileNames.push(fileName);
                     this.setState({
-                        // uploadedFileNames: uploadedFileNames,
                         isFormInvalid: false
                     });
                     this.renderFile(fileObj[i])
@@ -70,7 +70,7 @@ export default class UploadForm extends Component {
                   }    
       
               }
-            }else{
+            } else {
               this.setState({
                 fileLimitExceeded: true,
                 uploadedFileNames: []
@@ -86,15 +86,34 @@ export default class UploadForm extends Component {
         }               
       }
     
-    
       // Backend incorporation (basic pdb upload for now)
       onClickHandler = () => {
+        // If file doesn't exist returns
+        if (this.state.fileObject == null) return; 
+
+        // Vars passed to update App.js state, changes parent state to child state
+        var dataLoaded = this.state.dataLoaded;
+        var processedData = this.state.processedData;
+        var rows = this.state.rows;
+        var cols = this.state.cols;
+        this.props.handleSubmit(dataLoaded, rows, cols, processedData);
+
         const data = new FormData();
-        if (this.fileObject == null) return;
         for(var x = 0; x<this.state.fileObject.length; x++) {
           data.append('file', this.state.fileObject[x])
         }
-        axios.post(apiURL + '/upload', data, {
+        axios.post(apiURL + '/submit', data, {
+        }).then(res => {
+            if (res.status === 200) {
+              axios.get(apiURL+"/process")
+                  .then(rsp => rsp.data)
+                  .then(json => {
+                    this.setState({
+                      processedData: json,
+                      dataLoaded: false
+                    });
+                  });
+            }
         }).catch(err => console.log(err))
       }
     
@@ -110,9 +129,9 @@ export default class UploadForm extends Component {
 
     render() {
         return (
-        <form className="pdb-form">
+        <form className={this.props.className}>
             <FormGroup row>
-                <Label for="exampleFile" xs={6} sm={4} lg={2} size="lg">Upload pdb</Label>          
+                <Label for="exampleFile" xs={6} sm={4} lg={2} size="lg">Upload {this.props.fileType}</Label>          
                 <Col xs={4} sm={8} lg={10}>                                                     
                 <InputGroup>
                     <InputGroupAddon addonType="prepend">
@@ -122,22 +141,22 @@ export default class UploadForm extends Component {
                         </Button>
                     <input type="file" hidden onChange={this.fileHandler.bind(this)} ref={this.fileInput} multiple onClick={(event)=> { event.target.value = null }} style={{"padding":"10px"}} />      
                     <button type="button" className="btn btn-success btn-block" onClick={this.onClickHandler}>
-                    Upload
+                    Submit
                     </button>
                     </InputGroupAddon>
                     <Input type="text" className="form-control" value={this.state.uploadedFileNames} readOnly invalid={this.state.isFormInvalid || this.state.fileLimitExceeded} />                                              
                     <FormFeedback>    
                     <Fade in={this.state.isFormInvalid} tag="h6" style={{fontStyle: "italic"}}>
-                        Please select a pdb file only !
+                      {this.props.warningOne}
                     </Fade> 
                     <Fade in={this.state.fileLimitExceeded} tag="h6" style={{fontStyle: "italic"}}>
-                    Only 1 pdb file allowed!
+                      {this.props.warningTwo}
                   </Fade>                                                                       
                     </FormFeedback>
                 </InputGroup>     
                 </Col>                                                   
             </FormGroup>
         </form>
-        )
-    }
+      )
+  }
 }
