3766243285663fe7cde171931f75f21a8670c705
Aravind Venkateswaran <z5208102@ad.unsw.edu.au>
2020-10-27 21:29:01 +1100

    done initial config file

--- api/components/parseTwo.js
+++ api/components/parseTwo.js
@@ -1,3 +1,4 @@
+let config = require('./Config.json')
 const xlsxFile = require('read-excel-file/node');
 
 var start = exports.parse = async function parseData(file_path){
@@ -10,18 +11,18 @@ var start = exports.parse = async function parseData(file_path){
         var sheet_name_list = workbook.SheetNames;
         var ws = workbook.Sheets[sheet_name_list[0]]
         var range = XLSX.utils.decode_range(ws['!ref']);
-        range.s.r = 11; // skip the first 11 rows 
+        range.s.r = config.gpr.rowsToSkip; // skip header rows
         ws['!ref'] = XLSX.utils.encode_range(range);
         XLSX.utils.sheet_to_json(ws)
         var xlData = XLSX.utils.sheet_to_json(ws);
         xlData.forEach(data => {
             MicroArrData.push({ 
-                peptideSeq: data.ID,
-                proteinId:data.Name || "",
-                rawMean:data['F532 Mean'],
-                backgroundMean:data['B532 Mean'],
-                foregroundMedian:data['F532 Median - B532'],
-                snr:data['SNR 532']
+                peptideSeq: data[JSON.parse(JSON.stringify(config.gpr.sequence))],
+                proteinId:data[JSON.parse(JSON.stringify(config.gpr.name))] || "",
+                rawMean:data[JSON.parse(JSON.stringify(config.gpr.rawMean))],
+                backgroundMean:data[JSON.parse(JSON.stringify(config.gpr.backgroundMean))],
+                foregroundMedian:data[JSON.parse(JSON.stringify(config.gpr.foregroundMedian))],
+                snr:data[JSON.parse(JSON.stringify(config.gpr.snr))]
             })
         })
     }else{
@@ -40,37 +41,37 @@ var start = exports.parse = async function parseData(file_path){
         for(i in rows){
             for(j in rows[i]){
                 if(rows[i][j]!=null){
-                    if(String(rows[i][j]).match(/Peptide/ig)){
+                    if(String(rows[i][j]).match(new RegExp(JSON.parse(JSON.stringify(config.excel.sequence)),'ig'))){
                         let iter = 0;
                         for(let row=Number(i)+1;row<rows.length; row++){
                             MicroArrData[iter].peptideSeq = rows[row][j]
                             iter++;
                         }
-                    }else if(String(rows[i][j]).match(/Antigen\/Protein ID/gi)){
+                    }else if(String(rows[i][j]).match(new RegExp(JSON.parse(JSON.stringify(config.excel.name)),'ig'))){
                         let iter=0;
                         for(let row=Number(i)+1;row<rows.length; row++){
                             MicroArrData[iter].proteinId = rows[row][j]
                             iter++;
                         }
-                    }else if(String(rows[i][j]).match(/Raw Mean/gi)){
+                    }else if(String(rows[i][j]).match(new RegExp(JSON.parse(JSON.stringify(config.excel.rawMean)),'ig'))){
                         let iter=0;
                         for(let row=Number(i)+1;row<rows.length; row++){
                             MicroArrData[iter].rawMean = rows[row][j]
                             iter++;
                         }
-                    }else if(String(rows[i][j]).match(/Background Mean/gi)){
+                    }else if(String(rows[i][j]).match(new RegExp(JSON.parse(JSON.stringify(config.excel.backgroundMean)),'ig'))){
                         let iter=0;
                         for(let row=Number(i)+1;row<rows.length; row++){
                             MicroArrData[iter].backgroundMean = rows[row][j]
                             iter++;
                         }
-                    }else if(String(rows[i][j]).match(/Foreground Median/gi)){
+                    }else if(String(rows[i][j]).match(new RegExp(JSON.parse(JSON.stringify(config.excel.foregroundMedian)),'ig'))){
                         let iter=0;
                         for(let row=Number(i)+1;row<rows.length; row++){
                             MicroArrData[iter].foregroundMedian = rows[row][j]
                             iter++;
                         }
-                    }else if(String(rows[i][j]).match(/snr/gi)){
+                    }else if(String(rows[i][j]).match(new RegExp(JSON.parse(JSON.stringify(config.excel.snr)),'ig'))){
                         let iter=0;
                         for(let row=Number(i)+1;row<rows.length; row++){
                             MicroArrData[iter].snr = rows[row][j]
@@ -117,27 +118,23 @@ var start = exports.parse = async function parseData(file_path){
         tripleAveragedData.push({
             peptideSeq: ps,
             proteinId: pi,
-            data: [ {
+           // data: [ {
                 file: file_path,
                 rawMean: rm,
                 backgroundMean: bm,
                 foregroundMedian: fm,
                 snr:sr 
-            } ]
+            //} ]
         })
 
 
     })
-    //console.log(tripleAveragedData)
-    return tripleAveragedData;
+    console.log(tripleAveragedData)
+    //return tripleAveragedData;
 }
 
 
-
-start("./ige.gpr");
-
-
-
+//start("./ige.gpr") -> for debugging purposes
 
 
 
