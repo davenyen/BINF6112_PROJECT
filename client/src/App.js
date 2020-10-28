a3624f24646607bca6d49d93806d13ec0c527840
Leonie Dickson <leonie.dickson@gmail.com>
2020-10-28 21:57:30 +1100

    fix bugs from merging

--- client/src/App.js
+++ client/src/App.js
@@ -10,113 +10,21 @@ import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 // - convert to hooks 
 // - split into different components
 
-const apiURL = "http://localhost:8000";
+// const apiURL = "http://localhost:8000";
 
 export default class App extends Component {
   constructor(){
     super();
     this.state={
-        dataLoaded: false,
-        rows: null,
-        cols: null,
         processedData: null
     }
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-    // callAPI() {
-    // fetch("http://localhost:9000/testAPI")
-    //     .then(res => res.text())
-    //     .then(res => this.setState({ apiResponse: res }))
-    //     .catch(err => err);
-    // }
-
-    // componentDidMount() {
-    //     this.callAPI();
-    // }
-
-    // Loads and renders file to client
-    renderFile = (fileObj) => {
-        //just pass the fileObj as parameter
-        ExcelRenderer(fileObj, (err, resp) => {
-        if(err){
-            console.log(err);            
-        }
-        else{
-            this.setState({
-            dataLoaded: true,
-            cols: resp.cols,
-            rows: resp.rows
-            });
-        }
-        }); 
-    }
-
-  // Checks if file is valid
-  fileHandler = (event) => {    
-    if(event.target.files.length){
-        this.setState({
-            fileObject: event.target.files
-        });
-        let fileObj = event.target.files;
-        let uploadedFileNames = [];
-        for (var i = 0; i < fileObj.length; i++) {
-            let fileName = fileObj[i].name;
-            if(fileName.slice(fileName.lastIndexOf('.')+1) === "xlsx"|| fileName.slice(fileName.lastIndexOf('.')+1) === "gpr" ){
-              uploadedFileNames.push(fileName);
-              this.setState({
-                  // uploadedFileNames: uploadedFileNames,
-                  isFormInvalid: false
-              });
-              this.renderFile(fileObj[i])
-            } else{
-              this.setState({
-                  isFormInvalid: true,
-                  uploadedFileNames: []
-              })
-              break;
-            }    
-
-        }
-
-        //check for file extension and pass only if it is .xlsx/gpr and display error message otherwise
-        if (uploadedFileNames.length > 0) {
-          this.setState({
-            uploadedFileNames: uploadedFileNames.join(", ")
-          })
-        }
-    }               
-  }
-
-
-  // Backend incorporation
-  onClickHandler = () => {
-    const data = new FormData();
-    for(var x = 0; x<this.state.fileObject.length; x++) {
-      data.append('file', this.state.fileObject[x])
-    }
-    axios.post(apiURL + '/upload', data, {
-        
-    }).then(res => {
-        // console.log(res);
-        if (res.status === 200) {
-          axios.get(apiURL+"/process")
-              .then(rsp => rsp.data)
-              .then(json => {
-                this.setState({
-                  processedData: json,
-                  dataLoaded: false
-                });
-              });
-        }
-    }).catch(err => console.log(err))
-  }
-
-  toggle() {
+  handleSubmit(json) {
     this.setState({
-      processedData: processedData
-    });
-    console.log(this.state.processedData);
+      processedData: json
+    })
   }
 
   render() {
@@ -138,38 +46,7 @@ export default class App extends Component {
           </div> 
         </Tabs> 
         </div>
-        <form className="xlsx/gpr-form">
-          <FormGroup row>
-            <Label for="exampleFile" xs={6} sm={4} lg={2} size="lg">Upload xlsx/gpr</Label>          
-            <Col xs={4} sm={8} lg={10}>                                                     
-              <InputGroup>
-                <InputGroupAddon addonType="prepend">
-                    <Button color="info" style={{color: "white", zIndex: 0}} onClick={this.openFileBrowser.bind(this)}>
-                        <i className="cui-file"></i>
-                        Browse&hellip;
-                    </Button>
-                  <input type="file" hidden onChange={this.fileHandler.bind(this)} ref={this.fileInput} multiple onClick={(event)=> { event.target.value = null }} style={{"padding":"10px"}} />      
-                  <button type="button" class="btn btn-success btn-block" onClick={this.onClickHandler}>
-                      Upload
-                  </button>
-                </InputGroupAddon>
-                <Input type="text" className="form-control" value={this.state.uploadedFileNames} readOnly invalid={this.state.isFormInvalid} />                                              
-                <FormFeedback>    
-                  <Fade in={this.state.isFormInvalid} tag="h6" style={{fontStyle: "italic"}}>
-                    Please select a .xlsx/.gpr file only !
-                  </Fade>                                                                
-                </FormFeedback>
-              </InputGroup>     
-            </Col>                                                   
-            </FormGroup>
-        </form>
-        <UploadForm/>
-        {this.state.dataLoaded && 
-        <div className="output-table">
-          <Card body outline color="secondary" className="restrict-card">
-              <OutTable data={this.state.rows} columns={this.state.cols} tableClassName="ExcelTable2007" tableHeaderRowClass="heading" />
-          </Card>  
-        </div>}
+        <UploadForm handleSubmit={this.handleSubmit}/>
         {this.state.processedData && 
           <Table data={this.state.processedData} />
         }
