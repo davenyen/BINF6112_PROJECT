014a2f3eb791bb46ab3ab6944894bec59e912c1c
lianguizi <41506706+lianguizi@users.noreply.github.com>
2020-11-11 01:48:22 +0800

    hide the skeleton of graph and add description for asa colour

--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -202,7 +202,7 @@ export default class UploadForm extends Component {
         feature: {
           myFull: {
             show: true,
-            title: 'View in Full Screen',
+            title: '全屏查看',
             icon: 'path://M432.45,595.444c0,2.177-4.661,6.82-11.305,6.82c-6.475,0-11.306-4.567-11.306-6.82s4.852-6.812,11.306-6.812C427.841,588.632,432.452,593.191,432.45,595.444L432.45,595.444z M421.155,589.876c-3.009,0-5.448,2.495-5.448,5.572s2.439,5.572,5.448,5.572c3.01,0,5.449-2.495,5.449-5.572C426.604,592.371,424.165,589.876,421.155,589.876L421.155,589.876z M421.146,591.891c-1.916,0-3.47,1.589-3.47,3.549c0,1.959,1.554,3.548,3.47,3.548s3.469-1.589,3.469-3.548C424.614,593.479,423.062,591.891,421.146,591.891L421.146,591.891zM421.146,591.891',
             onclick: () => {
               const element = document.getElementById('vehicleProvince');
@@ -326,17 +326,19 @@ export default class UploadForm extends Component {
         {this.state.pdbFile &&
         <div className='visualisation-wrap' style={{display: "flex"}}>
           <div className={'chart-wrap'} id={'vehicleProvince'} style={{backgroundColor: 'white'}}>
-            <ButtonGroup justified>
+            {!!this.props.data && <><ButtonGroup justified>
               <Button active={chartType === 'median'}
-                      onClick={() => this.setState({chartType: 'median'})}>Foreground Median</Button>
+                      onClick={() => this.setState({chartType: 'median'})}>Median</Button>
               <Button active={chartType === 'snr'}
-                      onClick={() => this.setState({chartType: 'snr'})}>Calculated SNR</Button>
+                      onClick={() => this.setState({chartType: 'snr'})}>calculated SNR</Button>
               {this.isIncludeSRN() && <Button active={chartType === 'include'}
                                               onClick={() => this.setState({chartType: 'include'})}>Include
                 SNR</Button>}
             </ButtonGroup>
-            <ReactEcharts style={{height: '80%', minHeight: 320}} ref={this.myChart} echarts={echarts} notMerge={true}
-                          option={this.getChartOption()}/>
+              <ReactEcharts style={{height: '80%', minHeight: 320}} ref={this.myChart} echarts={echarts} notMerge={true}
+                            option={this.getChartOption()}/>                        
+            <p >  Red Colour: ASA is above 0.2;  Grey Colour: ASA is below 0.2</p>
+            </>}
           </div>
           <div
             onMouseEnter={this.changeScroll}
